4bced94c719ff3d1ded89fdf728994f5f6379278
William Y Wong <wwong1987@gmail.com>
2020-11-10 14:23:08 -0800

    completed knex queries

--- src/practice.js
+++ src/practice.js
@@ -5,7 +5,89 @@ const knex = require('knex');
 
 const knexInstance = knex({
   client: 'pg',
-  connection: process.env.DB_URL,
+  connection: 'postgresql://dunder_mifflin@localhost/knex-practice',
 });
 
-console.log('knex and driver installed correctly');
+// knexInstance
+//   .from('amazong_products')
+//   .select('*')
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+// Building queries
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery();
+
+console.log(qry);
+
+const searchTerm = 'holo';
+
+// Searching Amazong products
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+searchByProduceName('holo');
+
+// Paginating Amazong products
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+paginateProducts(2);
+
+// Filter Amazong products that have images
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+getProductsWithImages();
+
+// Find the most popular Whopipe videos
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+mostPopularVideosForDays(30);
