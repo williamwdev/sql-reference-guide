5da7f218925d7f84d5e8a7aa2aa92a2eb69d3035
William Y Wong <wwong1987@gmail.com>
2020-11-10 17:25:46 -0800

    searchItemsAfterDate function working

--- src/drills.js
+++ src/drills.js
@@ -9,23 +9,23 @@ const knexInstance = knex({
 
 // Get all items that contain text
 // takes one parameter which will be any string. function will query shopping_list table and select the rows which have a name that contains the searchTerm using a case insensitive match
-function searchTerm(query) {
+function searchByName(searchTerm) {
   knexInstance
     .select('*')
     .from('shopping_list')
-    .where('product_name', 'ILIKE', `%${query}%`)
+    .where('product_name', 'ILIKE', `%${searchTerm}%`)
     .then((result) => {
       console.log(result);
     });
 }
 
-// searchTerm('steak');
+// searchByName('steak');
 
 // Get all items paginated
 // takes one parameter which will be a number. Function will query the shopping_list table using knex methods and select the pageNumber page of rows paginated to 6 items per page
-function pageNumber(number) {
+function searchByPage(pageNumber) {
   const productsPerPage = 6;
-  const offset = productsPerPage * (number - 1);
+  const offset = productsPerPage * (pageNumber - 1);
   knexInstance
     .select('*')
     .from('shopping_list')
@@ -36,8 +36,24 @@ function pageNumber(number) {
     });
 }
 
-pageNumber(1);
+// searchByPage(1);
 
 // Get all items added after Date
+// take one parameter which will be a number representing a number of days. function will query the shopping_list table using knex methods and select the rows which have a date_added that is greater than daysAgo
+function searchItemsAfterDate(daysAgo) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where(
+      'date_added',
+      '>',
+      knexInstance.raw(`now() - '${daysAgo} days'::INTERVAL`)
+    )
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+searchItemsAfterDate(1);
 
 // get the total cost for each category
